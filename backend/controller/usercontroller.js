1ca79d40f1cb64f4483b8bf83b520da4030ffd8e
sai shashank <saishashank.pendyala@gmail.com>
2025-07-30 22:13:35 +0530

    added user controllers

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -1,6 +1,43 @@
- const db= require("../database");
-const getusers=(req,res)=>{
-    res.send('user');
+const db = require("../database");
+const bcrypt = require("bcrypt");
+const getusers = async (req, res) => {
+  try {
+    const query = "select * from users";
+    const result = await db.query(query);
+    return res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.send("error fetching users");
+  }
+};
+const getuserbyid = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = "select * from users where id=?";
+    const result = await db.query(query, [id]);
+    res.send(result);
+  } catch (err) {
+    console.log("user not found", err);
+    res.send("user not found");
+  }
 };
 
-module.exports={ getusers}
+const updateuser = async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+    const id = req.params.id;
+    const query = "update users set username=?, password=?, role=? where id=?";
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const result = await db.query(query, [username, hash, role, id]);
+    res.send({
+      msg: "user updated successfully",
+      user: result,
+    });
+  } catch (err) {
+    console.log("user not found", err);
+    res.send("user not found");
+  }
+};
+
+module.exports = { getusers, getuserbyid, updateuser };
--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,7 +1,13 @@
-const express=require('express');
-const router=express.Router();
-const {getusers} = require('../controller/usercontroller');
-const {protect,authorise}= require('../controller/authcontroller');
-router.get('/getusers',protect,authorise("admin"),getusers);
+const express = require("express");
+const router = express.Router();
+const {
+  getusers,
+  getuserbyid,
+  updateuser,
+} = require("../controller/usercontroller");
+const { protect, authorise } = require("../controller/authcontroller");
+router.get("/getusers", protect, authorise("admin", "user"), getusers);
+router.get("/getuser/:id", protect, authorise("admin", "user"), getuserbyid);
+router.patch("/updateuser/:id", protect, authorise("admin", "user"), updateuser);
 
-module.exports=router;
+module.exports = router;
